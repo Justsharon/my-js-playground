4ca51f3950a7b0158da78ce3292a96f7ab4b3d04
Justsharon <odiwasharon@gmail.com>
2025-02-08 17:33:29 +0300

    solve: insert items into array problems
    Signed-off-by: Justsharon <odiwasharon@gmail.com>

--- array/arrayleetcode.js
+++ array/arrayleetcode.js
@@ -89,3 +89,62 @@ const sortedSquares1 = function (nums) {
   return result.sort((a, b) => a - b);
 };
 console.log(sortedSquares1([-4, -1, 0, 3, 10]));
+
+//Duplicate zeros
+/*
+Given a fixed-length integer array, arr,
+duplicate each occurence of zero, shifting the remaining elements to the right
+Input: arr = [1,0,2,3,0,4,5,0]
+Output: [1,0,0,2,3,0,0,4]
+*/
+const duplicateZeros = function (arr) {
+  let arrayLength = arr.length;
+  for (let i = 0; i < arrayLength; i++) {
+    let element = arr[i];
+    let target = 0;
+
+    if (element === target) {
+      arr.splice(i, 0, target); // Insert duplicate zero at the same index
+      i++; // Skip over the newly inserted zero
+    }
+  }
+  arr.length = arrayLength;
+  // return arr.slice(arrayLength, arr.length); this returns the removed part so as to make array length remain the same even after duplication of zeros
+};
+let arr = [1, 0, 2, 3, 0, 4, 5, 0];
+duplicateZeros(arr);
+console.log(arr);
+
+/*
+//Merge Sorted Array
+you are given two integers arrays nums1 and nums2,
+sorted in a non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+To accommodate this, nums1 has a length of m + n,
+where the first m elements denote the elements that should be merged,
+and the last n elements are set to 0 and should be ignored. nums2 has a length of n
+*/
+const merge = function (nums1, m, nums2, n) {
+  let i = m - 1; //(last valid element in nums1)
+  let j = n - 1; // (last element in nums2)
+  let k = m + n - 1; //(last position in nums1)
+
+  while (j >= 0) {
+    //Compare nums1[i] and nums2[j], place the larger one at nums1[k].
+    if (i >= 0 && nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      //Move backwards to avoid overwriting elements in nums1
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+    }
+    k--;
+  }
+};
+
+let nums1 = [1, 2, 3, 0, 0, 0];
+let nums2 = [2, 5, 6];
+merge(nums1, 3, nums2, 3);
+console.log(nums1);
