32f32ca03ad301a296204d0d6126b257acdfeab1
Justsharon <odiwasharon@gmail.com>
2025-03-16 07:21:44 +0300

    feat: looping over arays in the dom
    Signed-off-by: Justsharon <odiwasharon@gmail.com>

---
+++ dom-for-loop.js
@@ -0,0 +1,50 @@
+let postsArray = [];
+
+function renderPosts() {
+  let html = "";
+  for (let post of postsArray) {
+    html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        `;
+  }
+  document.getElementById("blog-list").innerHTML = html;
+}
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+  .then((res) => res.json())
+  .then((data) => {
+    postsArray = data.slice(0, 5);
+    renderPosts();
+  });
+
+document.getElementById("new-post").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const postTitle = document.getElementById("post-title").value;
+  const postBody = document.getElementById("post-body").value;
+  const data = {
+    title: postTitle,
+    body: postBody,
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+    .then((res) => res.json())
+    .then((post) => {
+      /**
+       * Challenge: Use our new renderPosts function to clean up this code.
+       *
+       * Don't forget to update the postsArray variable first!
+       */
+      postsArray.unshift(post);
+      renderPosts();
+    });
+});
